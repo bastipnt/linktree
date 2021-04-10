071f43433a753dce93d0f00c2a1591a8ecd1c45e
litc0de <litc0de@posteo.org>
2021-04-10 20:29:49 +0100

    Updates

---
+++ component---src-pages-404-js-631de1075bf6951f7970.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{9616:function(e,t,a){"use strict";a.r(t);var n=a(7294),u=a(7198),l=a(7487);t.default=function(){return n.createElement(u.Z,null,n.createElement(l.Z,{title:"404: Not found"}),n.createElement("h1",null,"NOT FOUND"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-631de1075bf6951f7970.js.map
